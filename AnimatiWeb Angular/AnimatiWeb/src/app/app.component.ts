b959d54115377d2ef609c73c226e926426782c4d
Gabriel Nieto De Jesus <gabrielnietodejesus@gmail.com>
2024-05-13 08:28:39 -0300

    carga pagina set stikers

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SetStickersComponent } from './set-stickers/set-stickers.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, SetStickersComponent],
+  template: '  <app-set-stickers></app-set-stickers>',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ AnimatiWeb Angular/AnimatiWeb/src/app/set-stickers/set-stickers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-stickers',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-stickers.component.html',
+  styleUrl: './set-stickers.component.css'
+})
+export class SetStickersComponent {
+
+}
