5ce5f4e5e3eb669d73d72d8fafcbe75fd76978c6
Gabriel Nieto De Jesus <gabrielnietodejesus@gmail.com>
2024-05-13 11:53:37 -0300

    Arreglos varios

---
+++ AnimatiWeb Angular/AnimatiWeb/src/app/separadores/separadores.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-separadores',
+  standalone: true,
+  imports: [],
+  templateUrl: './separadores.component.html',
+  styleUrl: './separadores.component.css'
+})
+export class SeparadoresComponent {
+
+}
