87fa28b2598d1be5454ccf64b11e98d8f503027c
jonjonathanarias <jonjonathanarias1@gmail.com>
2024-05-13 23:53:33 -0300

    Acododando rutas del nav

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
@@ -3,16 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { NavComponent } from "./shared/nav/nav.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-
-  imports: [RouterOutlet, GalleryComponent, FooterComponent, LoginComponent,],
-
-
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, GalleryComponent, FooterComponent, LoginComponent, NavComponent]
 })
 export class AppComponent {
   title = 'AnimatiWeb';
--- AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
@@ -4,12 +4,18 @@ import { ContactoComponentComponent } from './contacto-component/contacto-compon
 import { CubecraftCityComponent } from './cubecraft-city/cubecraft-city.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegistroDeUsuariosComponent } from './registro-de-usuarios/registro-de-usuarios.component';
+import { SetStickersComponent } from './set-stickers/set-stickers.component';
+import { SeparadoresComponent } from './separadores/separadores.component';
 
 export const routes: Routes = [
     {path:"gallery", component:GalleryComponent},
     {path:"contacto", component:ContactoComponentComponent},
     {path:"cubecraft", component:CubecraftCityComponent},
-    {path:"registroUsuarios", component:RegistroDeUsuariosComponent},{path:'login', component:LoginComponent},
+    {path:"registroUsuarios", component:RegistroDeUsuariosComponent},
+    {path:'login', component:LoginComponent},
+    {path:'separadores', component:SeparadoresComponent},
+    {path:'set-stickers', component:SetStickersComponent},
+    
     {path:"", redirectTo:"/home", pathMatch:"full"}
 ]
 
