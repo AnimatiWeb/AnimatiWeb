e16a078199ca64a4a7f11b9f963b19bdad15146d
Looser3058 <134340805+Looser3058@users.noreply.github.com>
2024-05-13 22:47:37 -0300

    rehacer el componente cubecraft

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
+import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
 
 export const routes: Routes = [
-    {path:"gallery", component:GalleryComponent}
+    {path:"gallery", component:GalleryComponent},
+    {path:"contacto", component:ContactoComponentComponent},
 ];
