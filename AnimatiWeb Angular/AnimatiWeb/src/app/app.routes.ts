61923d7f57e70f17267a617e8d97abceea343326
Pala797 <106939529+Pala797@users.noreply.github.com>
2024-05-13 22:50:41 -0300

    componente nav

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {   path: 'nav',
+        component: NavComponent
+    },
+
+    {   path: 'footer',
+    component: FooterComponent
+    }
+
+];
