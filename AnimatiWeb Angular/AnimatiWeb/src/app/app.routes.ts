6b89faa50762c75e589852c701599f25fd64b39f
Looser3058 <134340805+Looser3058@users.noreply.github.com>
2024-05-13 23:05:12 -0300

    Eliminacion de carpeta nav; modificacion en registro de usuario

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { GalleryComponent } from './gallery/gallery.component';
 import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
+import { CubecraftCityComponent } from './cubecraft-city/cubecraft-city.component';
+import { RegistroDeUsuariosComponent } from './registro-de-usuarios/registro-de-usuarios.component';
 
 export const routes: Routes = [
     {path:"gallery", component:GalleryComponent},
     {path:"contacto", component:ContactoComponentComponent},
+    {path:"cubecraft", component:CubecraftCityComponent},
+    {path:"registroUsuarios", component:RegistroDeUsuariosComponent},
 ];
