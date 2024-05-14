cb20fc699c935984967b519fc0bc3d0c68069504
jonjonathanarias <jonjonathanarias1@gmail.com>
2024-05-14 00:08:52 -0300

    cargando pagina principal

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { FooterComponent } from './footer/footer.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { NavComponent } from "./shared/nav/nav.component";
+import { PaginaPrincipalComponent } from "./pagina-principal/pagina-principal.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, GalleryComponent, FooterComponent, LoginComponent, NavComponent]
+    imports: [RouterOutlet, GalleryComponent, FooterComponent, LoginComponent, NavComponent, PaginaPrincipalComponent]
 })
 export class AppComponent {
   title = 'AnimatiWeb';
