03b823c5005a17e23f0434f7fb37a3c985e0ce68
DalilaPalomeque <dalilapalomeque@mi.unc.edu.ar>
2024-06-02 12:25:46 -0300

    Creación Dashboard Admin

--- AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { LoginComponent } from './pages/auth/login/login.component';
@@ -7,13 +7,14 @@ import { NavComponent } from "./shared/nav/nav.component";
 import { PaginaPrincipalComponent } from "./pagina-principal/pagina-principal.component";
 import { CommonModule } from '@angular/common';
 import { QuienesSomosComponent } from './pages/auth/quienes-somos/quienes-somos.component';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, QuienesSomosComponent, GalleryComponent, FooterComponent, LoginComponent, NavComponent, PaginaPrincipalComponent, RouterOutlet]
+    imports: [CommonModule, QuienesSomosComponent, GalleryComponent, FooterComponent, LoginComponent, NavComponent, PaginaPrincipalComponent, RouterOutlet, RouterLink, HeaderComponent]
 })
 export class AppComponent {
   title = 'AnimatiWeb';
--- AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
+++ AnimatiWeb Angular/AnimatiWeb/src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { LoginComponent } from './pages/auth/login/login.component';
 import { RegistroDeUsuariosComponent } from './registro-de-usuarios/registro-de-usuarios.component';
 import { SetStickersComponent } from './set-stickers/set-stickers.component';
 import { SeparadoresComponent } from './separadores/separadores.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SamplepageComponent } from './samplepage/samplepage.component';
 
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 import { QuienesSomosComponent } from './pages/auth/quienes-somos/quienes-somos.component';
@@ -20,6 +22,8 @@ export const routes: Routes = [
     {path:'set-stickers', component:SetStickersComponent},
     {path:'', component:PaginaPrincipalComponent},
     {path:'Quien-somos', component:QuienesSomosComponent},
+    {path: '', title: 'Dashboard Page', component: DashboardComponent},
+    {path: 'samplepage', title: 'Sample Page', component: SamplepageComponent},
     
     {path:"", redirectTo:"/", pathMatch:"full"}
 ];
